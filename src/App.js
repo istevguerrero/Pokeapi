e96f8600b070fd096a39adf0b1d06a6ee28949bb
Steven Guerrero <imstevenguerrero@gmail.com>
2021-10-31 22:01:42 -0500

    Created Image Carousel

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Navbar from './components/navigation/Navbar';
-import Inicio from './components/pages/Inicio';
-import Pokemon from './components/pages/Pokemon';
-import Items from './components/pages/Items';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/navigation/Navbar";
+import Inicio from "./components/pages/Inicio";
+import Pokemon from "./components/pages/Pokemon";
+import Items from "./components/pages/Items";
+import Carousel from "./components/carousel/Carousel";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar>
-          <Switch>
-            <Route path="/" exact component={Inicio}/>
-            <Route path="/pokemon" component={Pokemon}/>
-            <Route path="/items" component={Items}/>
-          </Switch>
-        </Navbar>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/" exact component={Inicio} />
+          <Route path="/pokemon" component={Pokemon} />
+          <Route path="/items" component={Items} />
+        </Switch>
       </Router>
     </div>
   );
--- src/components/pages/Inicio.js
+++ src/components/pages/Inicio.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import Carousel from "../carousel/Carousel";
 
 const Inicio = () => {
-    return (
-        <div>
-            <h1>Esta es la pagina de Inicio</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Carousel />
+      <h1>Esta es la pagina de Inicio</h1>
+    </div>
+  );
+};
 
-export default Inicio
+export default Inicio;
